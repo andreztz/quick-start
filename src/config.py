648f5209cdcb5b5eef2c53f6bd89e53e5d55d46a
andreztz <andreztz@gmail.com>
2019-05-10 20:42:45 -0300

    remove codigo não usado

--- src/config.py
+++ src/config.py
@@ -1,6 +1,5 @@
 # Arquivo responsável por gerenciar as configurações do usuário.
 
-from configparser import ConfigParser
 from pathlib import Path
 
 from appdirs import user_config_dir
@@ -9,15 +8,8 @@ from appdirs import user_data_dir
 
 base_dir = Path(__file__).parent
 project_root = base_dir.parent
-
-sample_folder = base_dir / "sample"
-sample_zip = base_dir / "sample.zip"
 config_ini = project_root / "config.ini"
 current_work_directory = Path.cwd()
-workspace = Path.home() / "WORKSPACE"
+sample_zip = project_root / 'data/sample.zip'
 
-config_default = {"WORKSAPCE": workspace}
 
-config = ConfigParser(config_default)
-config.read(config_ini)
-default_config = dict(config["default"])
