99685bb9e0adc8c391e17b30246aec7ed8cc1fd2
andreztz <andreztz@gmail.com>
2019-05-10 23:16:40 -0300

    Fix: No such file or directory.

--- quickstart/config.py
+++ quickstart/config.py
@@ -10,4 +10,7 @@ base_dir = Path(__file__).parent
 project_root = base_dir.parent
 config_ini = project_root / "config.ini"
 current_work_directory = Path.cwd()
-sample_zip = project_root.joinpath("data/sample.zip")
+# sample_zip = base_dir.joinpath("data/sample.zip")
+
+user_data = Path(user_data_dir())
+sample_zip = user_data.joinpath("quick-start/data/sample.zip")
